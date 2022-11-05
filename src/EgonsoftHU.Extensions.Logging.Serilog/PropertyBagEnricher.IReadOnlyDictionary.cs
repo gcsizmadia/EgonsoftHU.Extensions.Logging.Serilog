60f77fc7c95038c8e8c2590f6ef56eb39f089146
Gabor Csizmadia <gcsizmadia@users.noreply.github.com>
2022-11-05 02:50:09 +0100

    Add IReadOnlyDictionary<string, object?> explicit interface implementation to PropertyBagEnricher.

---
+++ src/EgonsoftHU.Extensions.Logging.Serilog/PropertyBagEnricher.IReadOnlyDictionary.cs
@@ -0,0 +1,47 @@
+﻿// Copyright © 2022 Gabor Csizmadia
+// This code is licensed under MIT license (see LICENSE for details)
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EgonsoftHU.Extensions.Logging
+{
+    partial class PropertyBagEnricher : IReadOnlyDictionary<string, object?>
+    {
+        bool IReadOnlyDictionary<string, object?>.ContainsKey(string key)
+        {
+            return Properties.ContainsKey(key);
+        }
+
+        bool IReadOnlyDictionary<string, object?>.TryGetValue(string key, out object? value)
+        {
+            if (Properties.TryGetValue(key, out LogEventPropertyCreationInfo? logEventPropertyCreationInfo))
+            {
+                value = logEventPropertyCreationInfo.Value;
+                return true;
+            }
+
+            value = null;
+            return false;
+        }
+
+        object? IReadOnlyDictionary<string, object?>.this[string key] => Properties[key].Value;
+
+        IEnumerable<string> IReadOnlyDictionary<string, object?>.Keys => Properties.Keys;
+
+        IEnumerable<object?> IReadOnlyDictionary<string, object?>.Values => Properties.Values.Select(property => property.Value);
+
+        int IReadOnlyCollection<KeyValuePair<string, object?>>.Count => Properties.Count;
+
+        IEnumerator<KeyValuePair<string, object?>> IEnumerable<KeyValuePair<string, object?>>.GetEnumerator()
+        {
+            return ((IEnumerable<KeyValuePair<string, object?>>)Properties.ToDictionary(kvp => kvp.Key, kvp => kvp.Value.Value)).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)Properties.ToDictionary(kvp => kvp.Key, kvp => kvp.Value.Value)).GetEnumerator();
+        }
+    }
+}
